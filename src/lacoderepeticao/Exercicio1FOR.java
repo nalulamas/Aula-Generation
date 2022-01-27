e5b45c0510e530fa4ed255e66f1ea5c865ff666c
nalulamas <67602155+nalulamas@users.noreply.github.com>
2022-01-27 10:42:52 -0300

    exercicioFor

---
+++ src/lacoderepeticao/Exercicio1FOR.java
@@ -0,0 +1,18 @@
+//Informar todos os números de 1000 a 1999 que quando divididos por 11 obtemos resto = 5. (FOR)
+package lacoderepeticao;
+public class Exercicio1FOR {
+	
+public class FORexercicio1 {
+         public static void main (String args[]) {
+                             
+                for(int x=1000;x<=1999;x++)
+                {
+                  if ((x % 11) == 5)
+                  System.out.println("O resto da divisão de "+x+" é igual a 5");
+                }
+
+            }
+
+    }
+
+}
