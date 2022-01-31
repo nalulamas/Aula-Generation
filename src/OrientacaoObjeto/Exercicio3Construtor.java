30f6f82abaa4ba29125707bce30a1c105bd0d200
nalulamas <67602155+nalulamas@users.noreply.github.com>
2022-01-31 17:02:38 -0300

    Update Exercicio3Construtor.java

--- src/OrientacaoObjeto/Exercicio3Construtor.java
+++ src/OrientacaoObjeto/Exercicio3Construtor.java
@@ -1,25 +1,38 @@
+/*3) Crie uma classe produto eletrônico e apresente os atributos e métodos
+referentes esta classe, em seguida crie um objeto produto eletrônico,
+defina as instancias deste objeto e apresente as informações deste objeto
+no console. */
 package OrientacaoObjeto;
 
-public class Exercicio3Objeto {	
-		
-		public static void main(String[] args) {
-			Exercicio3Construtor cel1 = new Exercicio3Construtor();
-			Exercicio3Construtor cel2 = new Exercicio3Construtor();
+public class Exercicio3Construtor {
+	
 
-			cel1.cor = "Rosa";
-			cel1.apps = 25;
-			cel1.desligar();
-			cel1.bateria = 50f;
-			cel1.status();
-			cel1.situacao();
-			
-			System.out.println("_____________________________");
-			cel2.cor = "Azul";
-			cel2.apps = 99;
-			cel2.ligar();
-			cel2.bateria = 76f;
-			cel2.status();
-			cel2.situacao();
+	String cor;
+	float bateria;
+	int apps;
+	boolean ligado;
+	
+	public void ligar()
+	{
+		this.ligado=true;
+	}
+	public void desligar()
+	{
+		this.ligado=false;
 	}
 
+	public void situacao() {
+		if (this.ligado == true)
+			System.out.println("Celular ligado.");
+		else
+			System.out.println("Celular desligado. ");	
+	}
+		
+		public void status() {
+			System.out.println("Cor do celular: " + this.cor);
+			System.out.println("Celular está ligado? " + this.ligado);
+			System.out.println("Quantos apps?" + this.apps);
+			System.out.println("Porcentagem de bateria: " + this.bateria + "%");
+	
+	}
 }
