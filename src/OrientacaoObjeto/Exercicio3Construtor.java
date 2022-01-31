47c436eafe88471e74e27041321a5e273f7d7873
nalulamas <67602155+nalulamas@users.noreply.github.com>
2022-01-31 17:02:19 -0300

    Update Exercicio3Construtor.java

--- src/OrientacaoObjeto/Exercicio3Construtor.java
+++ src/OrientacaoObjeto/Exercicio3Construtor.java
@@ -1,5 +1,25 @@
 package OrientacaoObjeto;
 
-public class Exercicio3Construtor {
+public class Exercicio3Objeto {	
+		
+		public static void main(String[] args) {
+			Exercicio3Construtor cel1 = new Exercicio3Construtor();
+			Exercicio3Construtor cel2 = new Exercicio3Construtor();
+
+			cel1.cor = "Rosa";
+			cel1.apps = 25;
+			cel1.desligar();
+			cel1.bateria = 50f;
+			cel1.status();
+			cel1.situacao();
+			
+			System.out.println("_____________________________");
+			cel2.cor = "Azul";
+			cel2.apps = 99;
+			cel2.ligar();
+			cel2.bateria = 76f;
+			cel2.status();
+			cel2.situacao();
+	}
 
 }
